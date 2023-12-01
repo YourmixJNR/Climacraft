ea8cf919e461753743dfb517c9d74ce10238dab4
YourmixJNR <radmakson@gmail.com>
2023-12-01 20:18:12 +0100

    init

--- src/Component/SearchBox.jsx
+++ src/Component/SearchBox.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
 const SearchBox = () => {
   const [searchValue, setSearchValue] = useState("");
-  
+
   const handleSearchValue = (e) => {
-    setSearchValue(e.target.value)
-  }
+    setSearchValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchValue},uk&APPID=${process.env.REACT_APP_API_KEY}`);
+        const data = await response.json();
+        console.log(data)
+       
+      } catch (error) {
+        console.log("Error fetching...", error);
+      }
+    };
+
+    if (searchValue.trim() !== "") {
+      fetchWeather();
+    }
+
+  }, [searchValue]);
 
   return (
     <div>
@@ -15,8 +33,8 @@ const SearchBox = () => {
         <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
         <Form.Control
           type="text"
-          placeholder="Username"
-          aria-label="Username"
+          placeholder="Enter location"
+          aria-label="Location"
           aria-describedby="basic-addon1"
           value={searchValue}
           onChange={(e) => handleSearchValue(e)}
