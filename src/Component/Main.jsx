b3329bb08ab5aa9c20ccb352c9716ca33b3a3690
YourmixJNR <radmakson@gmail.com>
2023-12-02 19:23:26 +0100

    init

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 import Header from "./Layouts/Header";
-import SearchBox from "./SearchBox";
+import "../App.css";
 
 const Main = () => {
-  
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchValue = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const response = await fetch(
+          `http://api.openweathermap.org/data/2.5/weather?q=${searchValue},uk&APPID=${process.env.REACT_APP_API_KEY}`
+        );
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log("Error fetching...", error);
+      }
+    };
+
+    if (searchValue.trim() !== "") {
+      fetchWeather();
+    }
+  }, [searchValue]);
+
   return (
     <div>
       <Header />
-      <Container className="p-5 mb-4 bg-body-tertiary rounded-3">
+      <Container className="p-5 mb-4 bg-body-tertiary rounded-3 custom">
         <div className="container-fluid py-5">
           <h1 className="display-5 fw-bold">Custom Jumbotron</h1>
           <p className="col-md-8 fs-4">
@@ -16,7 +41,18 @@ const Main = () => {
             like the one in.
           </p>
           <hr />
-          <SearchBox />
+
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder="Enter location"
+              aria-label="Location"
+              aria-describedby="basic-addon1"
+              value={searchValue}
+              onChange={(e) => handleSearchValue(e)}
+            />
+          </InputGroup>
         </div>
       </Container>
     </div>
