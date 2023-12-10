0ab9b9980c14b490c3622e7e5ee6c843a6dc95fa
YourmixJNR <radmakson@gmail.com>
2023-12-10 01:41:35 +0100

    init

--- src/Component/WeatherResult.jsx
+++ src/Component/WeatherResult.jsx
@@ -5,26 +5,20 @@ import Card from "react-bootstrap/Card";
 
 const WeatherResult = ({ data }) => {
   if (!data || !data.weather || data.weather.length === 0) {
-    
-    return (
-      <Card style={{ width: "18rem" }}>
-        <Card.Body>
-          <Card.Title>No weather data available</Card.Title>
-        </Card.Body>
-      </Card>
-    );
+    return;
   }
 
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={`https://openweathermap.org/img/wn/${data.weather.icon}@2x.png`} />
+      <Card.Img
+        variant="top"
+        src={`https://openweathermap.org/img/wn/${data.weather.icon}@2x.png`}
+      />
       <Card.Body>
         <Card.Title>Weather Details</Card.Title>
         <Card.Text>
           {data.weather.map((item) => (
-            <div key={item.id}>
-              {item.description}
-            </div>
+            <div key={item.id}>{item.description}</div>
           ))}
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
