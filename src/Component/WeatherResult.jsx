8d6887a8b8131d53a547cba28be1df8dde17ae30
YourmixJNR <radmakson@gmail.com>
2023-12-14 05:11:05 +0100

    added spinner

--- src/Component/WeatherResult.jsx
+++ src/Component/WeatherResult.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Spinner from 'react-bootstrap/Spinner';
 
 const ShowWeather = ({ data }) => {
   if (!data || !data.weather || !data.main || data.weather.length === 0) {
-    return <div>Loading...</div>; 
+    return <Spinner animation="grow" />;
   }
 
   return (
